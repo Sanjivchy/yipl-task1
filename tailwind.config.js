4c9e5b3661c62e2c8f201d7363ce9986f934e008
xoonziip <sanjivchaudhary416@gmail.com>
2021-06-23 08:51:42 +0545

    template

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.html',
+    './src/**/*.js',
+
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      
+    },
+  },
+  variants: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
