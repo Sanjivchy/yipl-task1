9e5cdfea5732b5576f4329776a6e460d1be958df
xoonziip <sanjivchaudhary416@gmail.com>
2021-06-23 20:02:56 +0545

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,23 @@ module.exports = {
     './src/**/*.js',
 
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      
+      textColor:{
+        skin:{
+          base:'var(--color-text-base)',
+          muted:'var( --color-text-muted)',
+          header:'var(--color-text-header)',
+          darkmode:'var(--color-text-darkmode)'
+        }
+      },
+      backgroundColor:{
+        skin:{
+          primaryFill:'var(--color-primaryfill)',
+          secondaryFill:'var(--color-secondaryfill)'
+        }
+      },   
     },
   },
   variants: {
